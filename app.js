83fe657e6ca5904ca3733e8f40c0137e433d97c7
DKlaver <dklaver15@gmail.com>
2021-01-19 17:31:59 -0600

    Created a new function to call the randomItem function for each array to reduce the number of global variables.

--- app.js
+++ app.js
@@ -7,26 +7,39 @@ function randomItem (listOfItems) {
     return listOfItems[Math.floor(Math.random()*listOfItems.length)];
 }
 
-/*======================================================================*/
-
-//Randomly Selected Destination
-let destinations = ["Bali", "Hawaii", "Paris", "London", "Rome", "Cayman Islands", "Munich"];
-confirmRandom(destinations);
-
-/*======================================================================*/
-
-//Randomly Selected Restaurants
-let restaurants = ["Hard Rock Cafe", "McDonald's", "Taco Bell", "Chili's", "Ruth Chris"];
-let randomRestaurant = randomItem(restaurants);
+function itinerary (destination, restaurant, transportation, entertainment) {
+    let randomDesinations = randomItem(destination);
+    let randomRestaurant = randomItem(restaurant);
+    let randomTransport = randomItem(transportation);
+    let randomEntertainment = randomItem(entertainment);
+}
 
 /*======================================================================*/
 
-//Randomly Selected Mode of Transportation
+//Destination
+let destination = ["Bali", "Hawaii", "Paris", "London", "Rome", "Cayman Islands", "Munich"];
+
+/*======================================================================*/
+
+//Restaurants
+let restaurant = ["Hard Rock Cafe", "McDonald's", "Taco Bell", "Chili's", "Ruth Chris"];
+
+/*======================================================================*/
+
+//Transportation
 let transportation = ["Bus", "Train", "Taxi", "Airplane", "Boat"];
-let randomTransport = randomItem(transportation);
 
 /*======================================================================*/
 
-//Randomly Selected Form of Entertainment
+//Entertainment
 let entertainment = ["Movie", "Sight-seeing tour", "Opera", "Live Theater", "Dancing"];
-let randomEntertainment = randomItem(entertainment);
+
+
+/* 1. create an array of destinations
+2. create an array of restaurants
+3. create an array of transportation
+4. create an array of entertainment
+5. create a function that will select one item from an array at random
+6. if the user doesn't like the random selection chosen, they can re-select from that category
+7. if the user is happy with their trip, they can confirm it is "complete" (JS confirm box?)
+8. display the confirmed trip in the console  */
