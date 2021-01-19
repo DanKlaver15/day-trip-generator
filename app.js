b51d9d90f03a9789522751829d720ef167c98431
DKlaver <dklaver15@gmail.com>
2021-01-18 20:36:02 -0600

    Added values to arrays for each portion of the trip

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ function randomItem (listOfItems) {
     return listOfItems[Math.floor(Math.random()*listOfItems.length)];
 }
 
+/*======================================================================*/
+
+//Randomly Selected Destination
+let destinations = ["Bali", "Hawaii", "Paris", "London", "Rome", "Cayman Islands", "Munich"];
+confirmRandom(destinations);
+
+/*======================================================================*/
+
+//Randomly Selected Restaurants
+let restaurants = ["Hard Rock Cafe", "McDonald's", "Taco Bell", "Chili's", "Ruth Chris"];
+let randomRestaurant = randomItem(restaurants);
+
+/*======================================================================*/
+
+//Randomly Selected Mode of Transportation
+let transportation = ["Bus", "Train", "Taxi", "Airplane", "Boat"];
+let randomTransport = randomItem(transportation);
+
+/*======================================================================*/
+
+//Randomly Selected Form of Entertainment
+let entertainment = ["Movie", "Sight-seeing tour", "Opera", "Live Theater", "Dancing"];
+let randomEntertainment = randomItem(entertainment);
