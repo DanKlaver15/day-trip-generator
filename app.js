4d728dfe99fb6f5fce633adc2f3b453607a83bc3
DKlaver <dklaver15@gmail.com>
2021-01-18 20:33:29 -0600

    Created JavaScript function for choosing a random item from an array

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 "use strict";
+//Day Trip Generator
+
+//Function to randomly select from an array
+
+function randomItem (listOfItems) {
+    return listOfItems[Math.floor(Math.random()*listOfItems.length)];
+}
 
