56760339d6c7da1703bc53e3fbc4bc90a9eecaed
DKlaver <dklaver15@gmail.com>
2021-01-19 17:57:35 -0600

    Separated the the random selection of each section of the itinerary into its own function while beginning a new function to re-select if the user is unhappy with the intial selection

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 "use strict";
 //Day Trip Generator
+/* 1. create an array of destinations
+2. create an array of restaurants
+3. create an array of transportation
+4. create an array of entertainment
+5. create a function that will select one item from an array at random
+6. if the user doesn't like the random selection chosen, they can re-select from that category
+7. if the user is happy with their trip, they can confirm it is "complete" (JS confirm box?)
+8. display the confirmed trip in the console  */
 
 //Function to randomly select from an array
 
@@ -7,12 +15,27 @@ function randomItem (listOfItems) {
     return listOfItems[Math.floor(Math.random()*listOfItems.length)];
 }
 
-function itinerary (destination, restaurant, transportation, entertainment) {
-    let randomDesinations = randomItem(destination);
+function initialItinerary (destination, restaurant, transportation, entertainment) {
+    let randomDestinations = randomItem(destination);
     let randomRestaurant = randomItem(restaurant);
     let randomTransport = randomItem(transportation);
     let randomEntertainment = randomItem(entertainment);
+    console.log("Initial itinerary: " + randomDestinations + "/" + randomRestaurant + "/" + randomTransport + "/" + randomEntertainment);
 }
+
+/* function reselectItinerary() {
+    let finalDestination;
+    let finalRestaurant;
+    let finalTransport;
+    let finalEntertainment;
+    if (confirm(randomDesinations)) {
+        finalDestination = randomDestinations;
+    } 
+    else {
+        randomDestinations = randomItem(destination);
+        confirm(randomDestinations);
+    } 
+} */
 
 /*======================================================================*/
 
@@ -34,12 +57,6 @@ let transportation = ["Bus", "Train", "Taxi", "Airplane", "Boat"];
 //Entertainment
 let entertainment = ["Movie", "Sight-seeing tour", "Opera", "Live Theater", "Dancing"];
 
+/*======================================================================*/
 
-/* 1. create an array of destinations
-2. create an array of restaurants
-3. create an array of transportation
-4. create an array of entertainment
-5. create a function that will select one item from an array at random
-6. if the user doesn't like the random selection chosen, they can re-select from that category
-7. if the user is happy with their trip, they can confirm it is "complete" (JS confirm box?)
-8. display the confirmed trip in the console  */
+initialItinerary(destination, restaurant, transportation, entertainment);
